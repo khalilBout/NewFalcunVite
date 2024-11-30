aee702c4c1634606f7f9508944d2edc460df9e87
khalilBout <bouternikh.khalil@gmal.com>
2024-11-30 11:00:51 +0100

    update ui

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,7 +7,7 @@ import Printing from "../component/Printing/Printing";
 import Info from "../component/Info/Info";
 import Map from "../component/Map/Map";
 import Brand from "../component/Brand/Brand";
-import { Footer } from "../component/Footer/Footer";
+// import { Footer } from "../component/Footer/Footer";
 import Hero from "../component/Hero/Hero";
 
 const Index = () => {
